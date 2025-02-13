afb27988afdc154f3bcd8dabc7f1c187060ee873
shivansh <trivedishivansh1207@gmail.com>
2025-02-13 23:25:54 +0530

    Resolved lint errors

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,10 +1,10 @@
 import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+// import Search from '@/app/ui/search';
+// import Table from '@/app/ui/invoices/table';
+// import { CreateInvoice } from '@/app/ui/invoices/buttons';
+// import { lusitana } from '@/app/ui/fonts';
+// import { Suspense } from 'react';
+// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchCustomersPages, fetchFilteredCustomers } from '@/app/lib/data';
  
